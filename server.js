9ec3357ba3367f0a69fa0ce4ca77cf3c0cb276c0
Robert Albenda <ralbenda@gmail.com>
2022-02-26 21:49:28 -0500

    basic seed

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const Sequelize = require("sequelize");
 const db = new Sequelize(
   process.env.DATABSE_URL || "postgres://localhost/guitar_shop"
 );
+const express = require('express');
+const { addListener } = require("nodemon");
+const app = express();
+const port = process.env.PORT || 3000;
 
 const Guitar = db.define("guitar", {
   name: {
@@ -25,16 +29,30 @@ const Brand = db.define("brand", {
 })
 
 Guitar.belongsTo(Brand)
+Brand.hasMany(Guitar)
+
+
 
 const seedAndSync = async() => {
   try {
     await db.sync({ force: true });
     console.log("data is seeded");
+    app.listen(port, () => {
+      console.log(`listening on port ${port}`)
+    })
     const fender = await Brand.create({name:'Fender'});
     const gibson = await Brand.create({name:"Gibson"});
     const ibanez = await Brand.create({name:"Ibanez"});
-    const martin = await Brand.create({name:"Martin"})
-    const prs = await Brand.create({name:"PRS"})
+    const martin = await Brand.create({name:"Martin"});
+    const prs = await Brand.create({name:"PRS"});
+    const lesPaul = await Guitar.create({name:" les paul", brandId: gibson.id});
+    const stratocaster = await Guitar.create({name:" stratocaster", brandId: fender.id});
+    const jem = await Guitar.create({name:" jem", brandId: ibanez.id});
+    const sg = await Guitar.create({name:" sg", brandId: gibson.id});
+    const d28 = await Guitar.create({name:" d-28", brandId: martin.id});
+    const d42 = await Guitar.create({name:" d-42", brandId: martin.id});
+    
+
 }
   catch(ex) {
     console.log(ex)
