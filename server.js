989c014054ee10468290f6e30d959c24b709c05b
Robert Albenda <ralbenda@gmail.com>
2022-02-27 13:26:19 -0500

    front end up and running

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = new Sequelize(
 const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path')
+const path = require("path");
 
 const Guitar = db.define("guitar", {
   name: {
@@ -32,9 +32,9 @@ const Brand = db.define("brand", {
 Guitar.belongsTo(Brand);
 Brand.hasMany(Guitar);
 
-app.get('/', (req, res, next) => res.sendFile(path.join(__dirname,"index.html")));
-
-
+app.get("/", (req, res, next) =>
+  res.sendFile(path.join(__dirname, "index.html"))
+);
 
 app.get("/api/guitars", async (req, res, next) => {
   try {
@@ -56,28 +56,21 @@ app.get("/api/brands", async (req, res, next) => {
   }
 });
 
-app.get('/api/brands/:id', async (req,res,next) => {
-  try{
-      res.send(await Brand.findByPk(req.params.id, { include: Guitar}))
+app.get("/api/brands/:id", async (req, res, next) => {
+  try {
+    res.send(await Brand.findByPk(req.params.id, { include: Guitar }));
+  } catch (ex) {
+    next(ex);
   }
-  catch(ex) {
-    next(ex)
+});
+
+app.get("/api/guitars/:id", async (req, res, next) => {
+  try {
+    res.send(await Guitar.findByPk(req.params.id, { include: Brand }));
+  } catch (ex) {
+    next(ex);
   }
-})
-
-app.get('/api/guitars/:id', async (req,res,next) => {
-  try{
-      res.send(await Guitar.findByPk(req.params.id, { include: Brand}))
-  }
-  catch(ex) {
-    next(ex)
-  }
-})
-
-
-
-
-
+});
 
 const seedAndSync = async () => {
   try {
@@ -89,23 +82,27 @@ const seedAndSync = async () => {
     const martin = await Brand.create({ name: "Martin" });
     const prs = await Brand.create({ name: "PRS" });
     const lesPaul = await Guitar.create({
-      name: "les paul",
+      name: "Les Paul",
       brandId: gibson.id,
     });
+    const sg = await Guitar.create({ name: "SG", brandId: gibson.id });
     const stratocaster = await Guitar.create({
-      name: "stratocaster",
+      name: "Stratocaster",
       brandId: fender.id,
     });
     const telecaster = await Guitar.create({
-      name: "telecaster",
+      name: "Telecaster",
       brandId: fender.id,
     });
-    const jem = await Guitar.create({ name: "jem", brandId: ibanez.id });
-    const sg = await Guitar.create({ name: "sg", brandId: gibson.id });
-    const d28 = await Guitar.create({ name: "d-28", brandId: martin.id });
-    const d42 = await Guitar.create({ name: "d-42", brandId: martin.id });
+    const jem = await Guitar.create({
+      name: "Steve Vai Jem",
+      brandId: ibanez.id,
+    });
+
+    const d28 = await Guitar.create({ name: "D-28", brandId: martin.id });
+    const d42 = await Guitar.create({ name: "D-42", brandId: martin.id });
     const custom24 = await Guitar.create({
-      name: "custom 24",
+      name: "Custom 24",
       brandId: prs.id,
     });
     app.listen(port, () => {
